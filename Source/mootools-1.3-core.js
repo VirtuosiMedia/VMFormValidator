0922d8c8236fca87a3b89dd13048ce5af4f53e18
Virtuosi Media <contact@virtuosimedia.com>
2011-03-21 10:30:27 -0700

    Removed build data

--- Source/mootools-1.3-core.js
+++ Source/mootools-1.3-core.js
@@ -1,15 +1,5 @@
 /*
 ---
-MooTools: the javascript framework
-
-web build:
- - http://mootools.net/core/7c56cfef9dddcf170a5d68e3fb61cfd7
-
-packager build:
- - packager build Core/Core Core/Array Core/String Core/Number Core/Function Core/Object Core/Event Core/Browser Core/Class Core/Class.Extras Core/Slick.Parser Core/Slick.Finder Core/Element Core/Element.Style Core/Element.Event Core/Element.Dimensions Core/Fx Core/Fx.CSS Core/Fx.Tween Core/Fx.Morph Core/Fx.Transitions Core/Request Core/Request.HTML Core/Request.JSON Core/Cookie Core/JSON Core/DOMReady Core/Swiff
-
-/*
----
 
 name: Core
 
